7f86f177f3057f435ce101e56b8d63113dbddd5e
“riteshwaykole21” <riteshwaykole21@gmail.com>
2022-12-01 10:48:07 +0530

    [Ritesh] add: player Check For Next Steps

--- SnakeAndLadderSimmuator/Program.cs
+++ SnakeAndLadderSimmuator/Program.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 
-namespace SnakeAndLadderSimmulator
+namespace SnakeAndLadder
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome TO SNAKE AND LADDER Game");
-
+            SnakeAndLadder playerOne = new SnakeAndLadder();
+            playerOne.DieRolling();
+            playerOne.PlayerCheck();
             Console.ReadLine();
         }
     }
